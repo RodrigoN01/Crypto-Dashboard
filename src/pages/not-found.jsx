bb8b1556abbee98901b75acbee69ce016542f771
Rodrigo <rod_novaes@hotmail.com>
2025-09-21 16:20:25 +0200

    Add Routes to HomePage, AboutPage and NotFound

---
+++ src/pages/not-found.jsx
@@ -0,0 +1,44 @@
+import { Link } from "react-router";
+
+const NotFound = () => {
+  return (
+    <div style={styles.container}>
+      <h1 style={styles.title}>
+        404
+        <p style={styles.message}>
+          Oops! The page you're looking for does not exist
+        </p>
+        <Link to='/' style={styles.link}>
+          ← Go Back Home
+        </Link>
+      </h1>
+    </div>
+  );
+};
+
+const styles = {
+  container: {
+    textAlign: "center",
+    padding: "80px 20px",
+    color: "#fff",
+  },
+
+  title: {
+    fontSize: "72px",
+    marginBottom: "20px",
+  },
+
+  message: {
+    fontSize: "18px",
+    marginBottom: "30px",
+  },
+
+  link: {
+    textDecoration: "none",
+    color: "#007bff",
+    fontWeight: "bold",
+    fontSize: "16px",
+  },
+};
+
+export default NotFound;
